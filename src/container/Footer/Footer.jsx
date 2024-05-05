2ff327d627b11c8358f2edbdc9c920a35dae54dc
MandemGibson <themaingib@gmail>
2024-05-05 23:23:45 +0000

    Footer: Done with newsletter section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { FooterOverlay, Newsletter } from '../../components';
+import {FiFacebook, FiTwitter, FiInstagram} from "react-icons/fi"
 
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
+    <Newsletter/>
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact"></div>
+      <div className="app__footer-links_logo"></div>
+      <div className="app__footer-links_work"></div>
+    </div>
+    <div className="footer__copyright"></div>
   </div>
 );
 
